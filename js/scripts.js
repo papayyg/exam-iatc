c80be89311176d1e2128316fb2cc157710aeff0a
papayyg <aslan.mamedrzaev@gmail.com>
2023-05-06 04:10:50 +0400

    ideas

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,26 @@ const swiper = new Swiper(".swiper", {
         clickable: true,
     },
 });
+
+// const blocks = document.querySelectorAll(".ideas__item");
+
+// blocks.forEach(function (block) {
+//     block.addEventListener("click", function () {
+//         let content = block.querySelector(".ideas__content");
+//         let contentStyle = window.getComputedStyle(content);
+//         if (contentStyle.display === "none") {
+//             content.style.display = "block";
+//         } else {
+//             content.style.display = "none";
+//         }
+//     });
+// });
+
+const blocks = document.querySelectorAll(".ideas__item");
+
+blocks.forEach(function (block) {
+    block.addEventListener("click", function () {
+        let content = block.querySelector(".ideas__content");
+        content.classList.toggle("show");
+    });
+});
