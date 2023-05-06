d826445abd09488af03f175105bbe0df5ba36701
papayyg <aslan.mamedrzaev@gmail.com>
2023-05-06 13:22:31 +0400

    reviews

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const swiper = new Swiper(".swiper", {
+const swiperHeader = new Swiper(".headerSwiper", {
     allowTouchMove: false,
     loop: true,
     effect: "fade",
@@ -12,19 +12,19 @@ const swiper = new Swiper(".swiper", {
     },
 });
 
-// const blocks = document.querySelectorAll(".ideas__item");
-
-// blocks.forEach(function (block) {
-//     block.addEventListener("click", function () {
-//         let content = block.querySelector(".ideas__content");
-//         let contentStyle = window.getComputedStyle(content);
-//         if (contentStyle.display === "none") {
-//             content.style.display = "block";
-//         } else {
-//             content.style.display = "none";
-//         }
-//     });
-// });
+const swiperReviews = new Swiper(".reviewsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    speed: 1000,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 const blocks = document.querySelectorAll(".ideas__item");
 
